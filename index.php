49f30afaa3ce972a3f1ea4a309e7621ad915a6fc
MarleyS439 <marleysantos439@gmail.com>
2024-09-30 22:22:11 -0300

    Base em css

--- index.php
+++ index.php
@@ -15,14 +15,43 @@
     <!-- Twitter/X Meta Tags -->
 
     <!-- Stylesheet -->
-    <link rel="stylesheet" href="client/views/assets/">
+    <link rel="stylesheet" href="client/views/assets/css/index.css">
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="client/views/assets/icons/favicon.ico" type="image/x-icon">
 
     <!-- Title -->
     <title>Green House</title>
 </head>
 
 <body>
+    <div class="overlay-header"></div>
 
+    <header>
+        <nav>
+            <div class="logo">
+                <a href="#">Green House</a>
+            </div>
+
+            <div class="links">
+                <ul>
+                    <li><a href="#">Início</a></li>
+                    <li><a href="#">Sobre</a></li>
+                    <li><a href="#">Venda</a></li>
+                    <li><a href="#">Locação</a></li>
+                    <li><a href="#">Depoimentos</a></li>
+                </ul>
+            </div>
+
+            <div class="call">
+                <a href="#">
+                    <img src="client/views/assets/icons/person.svg" alt="">
+                    <span>Entrar</span>
+                </a>
+            </div>
+
+        </nav>
+    </header>
 </body>
 
 </html>
